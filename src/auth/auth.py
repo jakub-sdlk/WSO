7809fc3a05a52ed75377885ba8157029eac9f6cd
Jakub Sedlacek <jakub.sdlk@gmail.com>
2023-07-18 19:49:19 +0200

    create python variables to store form inputs in login and signup pages

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -3,10 +3,20 @@ from datetime import timedelta
 
 auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
 
+
 @auth.route("/login", methods=["POST", "GET"])
 def login():
-        return render_template("login.html")
+    e_mail = request.form.get("login_email")
+    password = request.form.get("login_password")
+
+    return render_template("login.html")
+
 
 @auth.route("/signup", methods=["POST", "GET"])
 def signup():
-        return render_template("signup.html")
+    first_name = request.form.get("signup_firstName")
+    last_name = request.form.get("signup_lastName")
+    e_mail = request.form.get("signup_email")
+    password = request.form.get("signup_password")
+
+    return render_template("signup.html")
