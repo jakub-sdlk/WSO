16afcefb6555e82ea2c125f2af6acf9fdf27fe7c
Jakub Sedlacek <jakub.sdlk@gmail.com>
2023-08-02 11:30:10 +0200

    add imports to auth.py file and remove the hyphen in e-mail

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, redirect, url_for, render_template, request, session
-from datetime import timedelta
+from flask import Blueprint, redirect, url_for, render_template, request, session, flash
+from main import db
+from models import User
 
 auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
 
@@ -14,9 +15,14 @@ def login():
 
 @auth.route("/signup", methods=["POST", "GET"])
 def signup():
-    first_name = request.form.get("signup_firstName")
-    last_name = request.form.get("signup_lastName")
-    e_mail = request.form.get("signup_email")
-    password = request.form.get("signup_password")
+    if request.method == 'POST':
+        first_name = request.form.get("signup_firstName")
+        last_name = request.form.get("signup_lastName")
+        email = request.form.get("signup_email")
+        password = request.form.get("signup_password")
+
+        email_exist = User.query.filter_by(email=email).first()
+        if email_exist:
+            flash('')
 
     return render_template("signup.html")
