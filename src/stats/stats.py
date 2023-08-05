706720b67a5df3682608606ccd2bc41fcce4c3c5
Jakub Sedlacek <jakub.sdlk@gmail.com>
2023-08-05 16:58:47 +0200

    welcome logged user on stats page

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 stats = Blueprint("stats", __name__, static_folder="static", template_folder="templates")
 
 @stats.route("/")
 @login_required
 def overview():
-    return "Your stats overview"
+    return f"<h1> Welcome, {current_user.first_name}! </h1> <p> Your stats:</p>"
