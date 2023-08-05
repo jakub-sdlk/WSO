ccfb7ed35dde2d40f9486bcd47d00120aa5675c5
Jakub Sedlacek <jakub.sdlk@gmail.com>
2023-08-05 16:54:45 +0200

    restrict acces to stats to logged users only

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
+from flask_login import login_required
 
 stats = Blueprint("stats", __name__, static_folder="static", template_folder="templates")
 
 @stats.route("/")
+@login_required
 def overview():
     return "Your stats overview"
