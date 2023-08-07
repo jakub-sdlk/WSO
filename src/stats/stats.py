c4e4349ee3a5d684b8079ee0c6c3ba5140939975
Jakub Sedlacek <jakub.sdlk@gmail.com>
2023-08-07 10:41:48 +0200

    move css and js files and change paths in html file

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
 stats = Blueprint("stats", __name__, static_folder="static", template_folder="templates")
@@ -6,4 +6,4 @@ stats = Blueprint("stats", __name__, static_folder="static", template_folder="te
 @stats.route("/")
 @login_required
 def overview():
-    return f"<h1> Welcome, {current_user.first_name}! </h1> <p> Your stats:</p>"
+    return render_template("index.html")
