5ff1114556d4e8b8a93e1ae88fe58d1b93e6e0f9
Jakub Sedlacek <jakub.sdlk@gmail.com>
2023-08-15 11:30:58 +0200

    add user_schedule table and class __repr__

--- src/models.py
+++ src/models.py
@@ -2,6 +2,11 @@ from db import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+user_schedule = db.Table('user_schedule',
+                         db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                         db.Column('schedule_id', db.Integer, db.ForeignKey('schedules.id'))
+                         )
+
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +16,10 @@ class Users(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     workout_sessions = db.relationship('WorkoutSessions', backref='users', passive_deletes=True)
+    registered_schedules = db.relationship('Schedules', secondary=user_schedule, backref='registered_users')
+
+    def __repr__(self):
+        return f'<User: {self.first_name} {self.last_name}>'
 
 
 class WorkoutSessions(db.Model):
@@ -24,10 +33,16 @@ class WorkoutSessions(db.Model):
     schedule_id = db.Column(db.Integer, db.ForeignKey('schedules.id'))
     position_in_schedule = db.Column(db.Integer)
 
+    def __repr__(self):
+        return f'<WorkoutSession: {self.id}>'
+
 
 class Schedules(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
+
+    def __repr__(self):
+        return f'<Schedule: {self.name}>'
 
 
 class Workouts(db.Model):
@@ -35,10 +50,5 @@ class Workouts(db.Model):
     name = db.Column(db.String(150))
     number_of_circles = db.Column(db.Integer)
 
-
-workouts_in_schedules = db.Table('workouts_in_schedules',
-                                 db.Column('workout_id', db.Integer, db.ForeignKey('workouts.id')),
-                                 db.Column('schedule_id', db.Integer, db.ForeignKey('schedules.id')),
-                                 db.Column('week', db.Integer),
-                                 db.Column('position_in_schedule', db.Integer)
-                                 )
+    def __repr__(self):
+        return f'<Workout: {self.name}>'
